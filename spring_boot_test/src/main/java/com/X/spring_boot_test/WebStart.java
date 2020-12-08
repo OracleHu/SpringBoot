4e643210ea747d95832ac90f913c956c03fba278
oraclehu <oraclehu@hotmail.com>
2020-12-08 22:23:06 +0800

    initialization

---
+++ spring_boot_test/src/main/java/com/X/spring_boot_test/WebStart.java
@@ -0,0 +1,18 @@
+package com.X.spring_boot_test;
+
+import org.springframework.boot.SpringApplication;
+
+//@RestController
+//@EnableAutoConfiguration
+public class WebStart {
+//	@RequestMapping("/")
+//	String home() {
+//		return "Hello World";
+//	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(RequestHandling.class, args);
+	}
+
+}
